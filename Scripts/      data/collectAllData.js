30c728fbeb777ac18c3fbefee1f5938d935607d0
A-Almandouh <aaa3h2@gmail.com>
2026-06-10 12:40:45 +0300

    Update collectAllData.js

--- Scripts/      data/collectAllData.js
+++ Scripts/      data/collectAllData.js
@@ -1,1 +1,38 @@
+// collectAllData.js
 
+async function collectAllData() {
+
+    //----------------------------------
+    // بيانات النظام
+    //----------------------------------
+
+    const generalData =
+        await extractGeneralData();
+
+    //----------------------------------
+    // بيانات الفورم الإضافى
+    //----------------------------------
+
+    const templateData =
+        collectTemplateFields();
+
+    //----------------------------------
+    // دمج البيانات
+    //----------------------------------
+
+    const data = {
+
+        ...generalData,
+
+        ...templateData
+
+    };
+
+    console.log(
+        "All Data",
+        data
+    );
+
+    return data;
+
+}
