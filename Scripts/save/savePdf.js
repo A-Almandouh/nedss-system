736a755c128456859f66e8f770363f8603368884
A-Almandouh <aaa3h2@gmail.com>
2026-06-10 12:51:59 +0300

    Update savePdf.js

--- Scripts/save/savePdf.js
+++ Scripts/save/savePdf.js
@@ -1,1 +1,55 @@
+// savePdf.js
 
+async function savePdf() {
+
+    const element =
+        document.body;
+
+    const options = {
+
+        margin: 0.5,
+
+        filename:
+            document.title +
+            ".pdf",
+
+        image: {
+
+            type: "jpeg",
+
+            quality: 1
+
+        },
+
+        html2canvas: {
+
+            scale: 2
+
+        },
+
+        jsPDF: {
+
+            unit: "in",
+
+            format: "a4",
+
+            orientation:
+                "portrait"
+
+        }
+
+    };
+
+    html2pdf()
+
+        .set(
+            options
+        )
+
+        .from(
+            element
+        )
+
+        .save();
+
+}
