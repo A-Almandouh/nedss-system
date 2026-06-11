6c3bd2c543cace940152199291e317847370fa43
A-Almandouh <aaa3h2@gmail.com>
2026-06-11 14:42:05 +0300

    Update saveToSheet.js

--- Scripts/save/saveToSheet.js
+++ Scripts/save/saveToSheet.js
@@ -36,7 +36,15 @@ async function saveToGovernorateSheet() {
 
     const data =
         await collectAllData();
+const result = splitData(data);
 
+console.log("GENERAL DATA");
+
+console.table(result.generalData);
+
+console.log("DISEASE DATA");
+
+console.table(result.diseaseData);
     console.log(
         "ALL DATA",
         data
