519d1ec9ff49c476a1336a8640f41b5e8b1da124
A-Almandouh <aaa3h2@gmail.com>
2026-06-11 09:05:29 +0300

    Create saveCsv.js

---
+++ Scripts/save/saveCsv.js
@@ -0,0 +1,113 @@
+console.log("saveCsv loaded");
+
+async function saveCsv() {
+
+    try {
+
+        const data =
+            collectAllData();
+
+        if (!data) {
+
+            alert(
+                "لا توجد بيانات"
+            );
+
+            return;
+        }
+
+        const headers =
+            Object.keys(data);
+
+        const values =
+            Object.values(data);
+
+        const escapeCsv =
+            value => {
+
+                const str =
+                    String(
+                        value ?? ""
+                    );
+
+                return '"' +
+                    str.replace(
+                        /"/g,
+                        '""'
+                    ) +
+                    '"';
+
+            };
+
+        const csv =
+            "\uFEFF" +
+            headers.map(
+                escapeCsv
+            ).join(",") +
+            "\r\n" +
+            values.map(
+                escapeCsv
+            ).join(",");
+
+        const blob =
+            new Blob(
+                [csv],
+                {
+                    type:
+                    "text/csv;charset=utf-8"
+                }
+            );
+
+        const url =
+            URL.createObjectURL(
+                blob
+            );
+
+        const a =
+            document.createElement(
+                "a"
+            );
+
+        a.href = url;
+
+        a.download =
+            (
+                data.PatientName ||
+                data.CaseID ||
+                "Case"
+            ) +
+            ".csv";
+
+        document.body.appendChild(
+            a
+        );
+
+        a.click();
+
+        a.remove();
+
+        setTimeout(
+            () => {
+
+                URL.revokeObjectURL(
+                    url
+                );
+
+            },
+            1000
+        );
+
+    }
+    catch (err) {
+
+        console.error(
+            err
+        );
+
+        alert(
+            err.message
+        );
+
+    }
+
+}
