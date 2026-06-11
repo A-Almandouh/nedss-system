aeaa2dd445d394be0a9e1dbf56a024f1ebaf0132
A-Almandouh <aaa3h2@gmail.com>
2026-06-11 08:24:09 +0300

    Update saveToSheet.js

--- Scripts/save/saveToSheet.js
+++ Scripts/save/saveToSheet.js
@@ -1,1 +1,32 @@
-console.log("saveToSheet loaded");
+console.log(
+    "saveToSheet loaded"
+);
+
+async function saveToDepartmentSheet() {
+
+    console.log(
+        "saveToDepartmentSheet"
+    );
+
+    const data =
+        await collectAllData();
+
+    console.log(data);
+
+    return;
+}
+
+
+async function saveToGovernorateSheet() {
+
+    console.log(
+        "saveToGovernorateSheet"
+    );
+
+    const data =
+        await collectAllData();
+
+    console.log(data);
+
+    return;
+}
