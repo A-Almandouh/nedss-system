f4cc9b7f63dc7d9cb67d57fd965fb01f6ecdb879
A-Almandouh <aaa3h2@gmail.com>
2026-06-13 14:31:23 +0300

    Update testSheet.js

--- Scripts/save/testSheet.js
+++ Scripts/save/testSheet.js
@@ -2,40 +2,45 @@ console.log("testSheet loaded");
 
 async function testSheet() {
 
-    const body = {
-
-        spreadsheetId:
-            "1g8NVjns3UNfURYebKkMBI33XB4BJUnDZJ3I6372J64M",
-
-        sheetName:
-            "G-Data",
-
-        data: {
-
-            CaseID: "TEST-001",
-
-            PatientName: "أحمد حمدى",
-
-            DiseaseID: "1",
-
-            Phone: "01000000000"
-
-        }
-
-    };
-
     const response = await fetch(
+
         "https://script.google.com/macros/s/AKfycbzbasd78UdrLoqpPgvtmBBb3YOX43A0bGcPgORXLRgJYwxX_7oZGZCaDfOppZdRgR9z/exec",
+
         {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(body)
+            method: "POST",
+
+            body: JSON.stringify({
+
+                spreadsheetId:
+                    "1g8NVjns3UNfURYebKkMBI33XB4BJUnDZJ3I6372J64M",
+
+                sheetName:
+                    "TEST",
+
+                data: {
+
+                    CaseID:
+                        "123456",
+
+                    PatientName:
+                        "Ahmed",
+
+                    DiseaseID:
+                        "1",
+
+                    Phone:
+                        "01000000000"
+
+                }
+
+            })
+
         }
+
     );
 
-    const result = await response.text();
+    const result =
+        await response.json();
 
     console.log(result);
 
